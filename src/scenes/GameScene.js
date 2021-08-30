9ebe2430eccee0a56048acdb84b2a1c2dfc7c9c5
Jeremy Dryden <jeremy.dryden@gmail.com>
2021-08-30 19:02:28 -0400

    player and movement added, uploading for testing on different machine

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,17 +1,23 @@
 import Phaser from "phaser";
+const GROUND_KEY = "ground";
+const DUDE_KEY = "dude";
+const STAR_KEY = "star";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super("game-scene");
+
+    this.player = undefined;
+    this.cursors = undefined;
   }
 
   preload() {
     this.load.image("sky", "assets/sky.png");
-    this.load.image("ground", "assets/platform.png");
-    this.load.image("star", "assets/star.png");
+    this.load.image(GROUND_KEY, "assets/platform.png");
+    this.load.image(STAR_KEY, "assets/star.png");
     this.load.image("bomb", "assets/bomb.png");
 
-    this.load.spritesheet("dude", "assets/dude.png", {
+    this.load.spritesheet(DUDE_KEY, "assets/dude.png", {
       frameWidth: 32,
       frameHeight: 48,
     });
@@ -19,6 +25,89 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, "sky");
-    this.add.image(400, 300, "star");
+
+    const platforms = this.createPlatforms();
+    this.player = this.createPlayer();
+    // const stars = this.createStars();
+
+    this.physics.add.collider(this.player, platforms);
+    // this.physics.add.collider(stars, platforms);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      console.log("left");
+      this.player.setVelocityX(-160);
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      console.log("right");
+      this.player.setVelocityX(160);
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+  }
+
+  createPlatforms() {
+    const platforms = this.physics.add.staticGroup();
+
+    // lowest ground level
+    platforms.create(400, 568, GROUND_KEY).setScale(2).refreshBody();
+
+    // three platforms
+    platforms.create(600, 400, GROUND_KEY);
+    platforms.create(50, 250, GROUND_KEY);
+    platforms.create(750, 220, GROUND_KEY);
+
+    return platforms;
+  }
+
+  createPlayer() {
+    const player = this.physics.add.sprite(100, 450, DUDE_KEY);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNames(DUDE_KEY, { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: DUDE_KEY, frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    return player;
+  }
+
+  // createStars() {
+  //   const stars = this.physics.add.group({
+  //     key: STAR_KEY,
+  //     repeat: 11,
+  //     setXY: { x: 12, y: 0, stepX: 70 },
+  //   });
+
+  //   stars.children.iterate((child) => {
+  //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  //   });
+
+  //   return stars;
+  // }
 }
